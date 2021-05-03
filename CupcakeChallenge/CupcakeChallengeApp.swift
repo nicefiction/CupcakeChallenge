ab66056b51d6446c49f3c72aa253d9a08e7ac47a
Olivier Van hamme <nicefiction@gmail.com>
2021-05-03 16:40:56 +0200

    Initial Commit

---
+++ CupcakeChallenge/CupcakeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeChallengeApp.swift
+//  CupcakeChallenge
+//
+//  Created by Olivier Van hamme on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
